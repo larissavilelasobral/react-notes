14b3ddaa228e3b8960efe6ae9819f57a59ef5ade
Larissa Vilela <larissavilelasobral@gmail.com>
2021-11-21 10:52:59 -0300

    Feat: add form

---
+++ src/Components/CardNota.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const CardNota = () => {
+  return (
+    <section>
+      <header>
+        <h3>Título</h3>
+      </header>
+      <p>Escreva sua nota</p>
+    </section>
+  );
+}
+
+export default CardNota;
---
+++ src/Components/ListaDeNotas.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CardNota from "./CardNota";
+
+const ListaDeNotas = () => {
+  return (
+    <ul>
+      <li>
+        <CardNota />
+      </li>
+      <li>
+        <CardNota />
+      </li>
+      <li>
+        <CardNota />
+      </li>
+    </ul>
+  );
+}
+
+export default ListaDeNotas;
